3dcd4e83809f32c56a781a772f0be9da7cb4a170
jesus <jsanjuan15@cuc.edu.co>
2017-10-31 19:04:38 -0500

    Final

---
+++ CarroMaterial-master/app/src/main/java/com/example/android/carrosmaterial/Datos.java
@@ -0,0 +1,48 @@
+package com.example.android.carrosmaterial;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+
+
+
+
+public class Datos {
+
+
+
+    private static String db = "Carros";
+    private static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+
+
+
+    private static ArrayList<Carro> carros = new ArrayList();
+
+    public static ArrayList<Carro> obtenerCarros() {
+        return carros;
+    }
+
+    public static void guardarCarro(Carro c) {
+
+
+        c.setId(databaseReference.push().getKey());
+        databaseReference.child(db).child(c.getId()).setValue(c);
+    }
+
+    public static void eliminarCarro(Carro c){
+        databaseReference.child(db).child(c.getId()).removeValue();
+    }
+
+
+
+    public static void editarCarro(Carro c){
+        databaseReference.child(db).child(c.getId()).setValue(c);
+    }
+
+    public static void setCarros(ArrayList<Carro> car){
+        carros = car;
+    }
+}
---
+++ CarroMaterial-master/app/src/main/java/com/example/android/carrosmaterial/Principal.java
@@ -0,0 +1,121 @@
+package com.example.android.carrosmaterial;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class
+
+
+
+
+
+
+Principal extends AppCompatActivity implements  AdaptadorCarro.OnCarroClickListener{
+
+
+
+    private RecyclerView listado;
+    private ArrayList<Carro> carros;
+    private Resources res;
+    private AdaptadorCarro adapter;
+    private LinearLayoutManager llm;
+    private Intent i;
+    private DatabaseReference databaseReference;
+    private final String BD = "Carros";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listado = (RecyclerView)findViewById(R.id.lstOpciones);
+
+
+
+        res = this.getResources();
+
+        carros = new ArrayList<>();
+
+        llm = new LinearLayoutManager(this);
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+
+        adapter = new AdaptadorCarro(this.getApplicationContext(), carros, this);
+
+        listado.setLayoutManager(llm);
+        listado.setAdapter(adapter);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(BD).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                carros.clear();
+
+                if (dataSnapshot.exists()){
+                    for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                        Carro p = snapshot.getValue(Carro.class);
+                        carros.add(p);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                Datos.setCarros(carros);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                i = new Intent(Principal.this, CrearCarro.class);
+                startActivity(i);
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public void onCarrosClick(Carro c) {
+        Intent i = new Intent(Principal.this, DetalleCarro.class);
+        Bundle b = new Bundle();
+
+        b.putString("id", c.getId());
+        b.putString("placa",c.getPlaca());
+        b.putInt("marca",c.getMarca());
+        b.putInt("modelo",c.getModelo());
+        b.putInt("color",c.getColor());
+        b.putInt("precio",c.getPrecio());
+        b.putInt("foto",c.getFoto());
+
+        i.putExtra("datos",b);
+        startActivity(i);
+    }
+}
